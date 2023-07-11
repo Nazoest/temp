8bfd279e3d1305a55d823111674e7341958bea82
Nathan <nathanmacharia115@gmail.com>
2023-07-11 13:10:45 +0300

    second commits

--- index.js
+++ index.js
@@ -19,8 +19,56 @@ require('./7-mind-grenade')
 //-built in modules:::1.os 2.Path 3.fs 4.http.....
 
 //http module basics of course
-const _ =require('lodash')
+/* const _ =require('lodash')
 
 const items=[1,[2,[3,[3,[4]]]]]
 const newItems=_.flattenDeep(items)
 console.log(newItems)
+console.log("hello world") */
+
+
+//
+/* console.log('first')
+setTimeout(()=>{
+    console.log('second')
+},0)
+console.log('third')
+ */
+const { readFile, writeFile } = require('fs');
+const { resolve } = require('path');
+const util=require('util')
+const readFilePromise=util.promisify(readFile)
+const writeFilePromise=util.promisify(writeFile)
+
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+/* getText('./content/first.txt')
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  }); */
+  
+  const start=async()=>{
+    try{
+    const first= await readFilePromise('./content/first.txt','utf-8')
+    const second=await readFilePromise('./content/second.txt','utf-8')
+    await writeFilePromise('./content/result-mind-grenade.txt',`This is AWESOME:${first}::${second},{flag: 'a'}`)
+    console.log(first,second)
+}catch(error){
+    console.log(error)
+}
+    
+  }
+  start()
